4fbd7c89fa2f66ab4c2e4ddda110d591934123f5
ain2108 <ain.jesape@gmail.com>
2016-07-24 21:15:09 -0400

    Now it works. Check out sick recursion in help.ml

--- src/help.ml
+++ src/help.ml
@@ -3,8 +3,23 @@ open Ast
 let rec get_vardecls vars = function
 	| [] -> List.rev vars
 	| hd::tl -> (match hd with
-		| Expr(Localdecl(typname, name)) -> get_vardecls ((typname, name)::vars) tl
-		| _ -> get_vardecls vars tl)
-
+		| Expr(Localdecl(typname, name)) -> 
+			print_string " Expr";
+			get_vardecls ((typname, name)::vars) tl
+		| Block(slist) -> print_string " Block";
+						  (match slist with
+			| [] -> get_vardecls vars tl
+			| hd1::tl1 ->
+				get_vardecls
+					(get_vardecls (get_vardecls vars [hd1]) tl1)
+					tl1)
+		| If(e, s1, s2) -> 
+			print_string " If";
+			get_vardecls (get_vardecls (get_vardecls vars [s1]) [s2]) tl
+		| For(_, _, _, s) -> 
+			get_vardecls (get_vardecls vars [s]) tl
+		| While(e, s) -> 
+			get_vardecls (get_vardecls vars [s]) tl
+		| _ -> get_vardecls vars tl )
 
 ;;
