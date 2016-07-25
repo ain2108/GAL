eb53b37005a8ecae788d00b17be7d756391a920c
ain2108 <ain.jesape@gmail.com>
2016-07-24 22:43:05 -0400

    Checks if builtins were redefined

--- src/semant.ml
+++ src/semant.ml
@@ -7,10 +7,16 @@ open Ast;;
 
 module StringMap = Map.Make(String);;
 
+(* Error messages of the exceptions *)
 let dup_global_exp = " duplicate global ";;
 let dup_local_exp = " duplicate local ";;
 let dup_formal_exp = " duplicate formal arg ";;
-let dup_func_exp = " duplicate function name "
+let dup_func_exp = " duplicate function name ";;
+let builtin_decl_exp = " cannot redefine ";;
+
+(* Names of built in functions can be added below *)
+let builtins_list =
+	["print"; "length"; "source"; "dest"; "pop"];;
 
 (* Static semantic checker of the program. Will return void 
    on success. Raise an exception otherwise. Checks first the
@@ -78,6 +84,17 @@ let rec purify_exp_list result = function
 	| _::tl -> purify_exp_list result tl
 ;;
 
+(* List of built ins is the implicit argument here*)
+let rec check_builtins_defs exp_list expmsg funcs = function
+	[] -> List.rev exp_list
+	| hd::tl -> 
+		if (List.mem hd funcs) then 
+			let exp = expmsg ^ hd in
+			check_builtins_defs (exp::exp_list) expmsg funcs tl
+		else 
+			check_builtins_defs exp_list expmsg funcs tl
+;;
+
 
 (* The thing that does all the checks *)
 let check (globals, funcs) = 
@@ -102,6 +119,13 @@ let check (globals, funcs) =
 		dup_func_exp 
 		(List.map (fun n -> n.fname) funcs) 
 		"")::exp 
+
+	(* Check if built ins were redefined *)
+	in let exp = (check_builtins_defs
+		exp
+		builtin_decl_exp
+		(List.map (fun n -> n.fname) funcs)
+		builtins_list)
 
 	(* Get rid of elements containing empty sstring *)
 	in purify_exp_list [] exp
