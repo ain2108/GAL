ce405855868347333899cbcf6019651a43b2211d
A.J <ajfthr@gmail.com>
2016-07-31 16:52:36 -0400

    testall.sh converted back to its origin and working now.

--- testall.sh
+++ testall.sh
@@ -1,11 +1,32 @@
-files="tests/*fail_*"
+#!/bin/sh
 
+# Regression testing script for MicroC
+# Step through a list of files
+#  Compile, run, and check the output of each expected-to-work test
+#  Compile and check the error of each expected-to-fail test
+
+# Path to the LLVM interpreter
+#LLI="lli"
+LLI="/usr/local/opt/llvm/bin/lli"
+
+# Path to the microc compiler.  Usually "./microc.native"
+# Try "_build/microc.native" if ocamlbuild was unable to create a symbolic link.
 GAL="./src/gal"
+#GAL="_build/microc.native"
+
+# Set time limit for all operations
+ulimit -t 30
 
 globallog=testall.log
+rm -f $globallog
+error=0
+globalerror=0
+
+keep=0
 
 Usage() {
     echo "Usage: testall.sh [options] [.gal files]"
+    echo "-k    Keep intermediate files"
     echo "-h    Print this help"
     exit 1
 }
@@ -18,6 +39,19 @@ SignalError() {
     echo "  $1"
 }
 
+# Compare <outfile> <reffile> <difffile>
+# Compares the outfile with reffile.  Differences, if any, written to difffile
+Compare() {
+    generatedfiles="$generatedfiles $3"
+    echo diff -b $1 $2 ">" $3 1>&2
+    diff -b "$1" "$2" > "$3" 2>&1 || {
+    SignalError "$1 differs"
+    echo "FAILED $1 differs from $2" 1>&2
+    }
+}
+
+# Run <args>
+# Report the command, run it, and report any errors
 Run() {
     echo $* 1>&2
     eval $* || {
@@ -26,30 +60,8 @@ Run() {
     }
 }
 
-Compare() {
-    generatedfiles="$generatedfiles $3"
-    #echo diff -b $1 $2 ">" $3 1>&2
-    diff -b "$1" "$2" > "$3" 2>&1 || {
-    SignalError "$1 differs"
-    cat "$3"
-    echo "FAILED $1 differs from $2" 1>&2
-    }
-}
-
-while getopts kdpsh c; do
-    case $c in
-    h) # Help
-        Usage
-        ;;
-    esac
-done
-
-rm -f $globallog
-error=0
-globalerror=0
-
-keep=0
-
+# RunFail <args>
+# Report the command, run it, and expect an error
 RunFail() {
     echo $* 1>&2
     eval $* && {
@@ -62,8 +74,8 @@ RunFail() {
 Check() {
     error=0
     basename=`echo $1 | sed 's/.*\\///
-                             s/.mc//'`
-    reffile=`echo $1 | sed 's/.mc$//'`
+                             s/.gal//'`
+    reffile=`echo $1 | sed 's/.gal$//'`
     basedir="`echo $1 | sed 's/\/[^\/]*$//'`/."
 
     echo -n "$basename..."
@@ -74,7 +86,7 @@ Check() {
     generatedfiles=""
 
     generatedfiles="$generatedfiles ${basename}.ll ${basename}.out" &&
-    Run "$MICROC" "<" $1 ">" "${basename}.ll" &&
+    Run "$GAL" "<" $1 ">" "${basename}.ll" &&
     Run "$LLI" "${basename}.ll" ">" "${basename}.out" &&
     Compare ${basename}.out ${reffile}.out ${basename}.diff
 
@@ -92,25 +104,25 @@ Check() {
     fi
 }
 
-CheckFail()
-{
+CheckFail() {
     error=0
-    basename_first=`echo $1 | sed 's/.*\\///'`
-    y=${basename_first%.*}
-    basename=`echo ${y##*/}`
+    basename=`echo $1 | sed 's/.*\\///
+                             s/.gal//'`
     reffile=`echo $1 | sed 's/.gal$//'`
-    basedir="`echo $1 | sed 's/\/[^\/]*$//'`/"
+    basedir="`echo $1 | sed 's/\/[^\/]*$//'`/."
 
-    echo -n "basename: $basename..."
-    #echo -n "reffile: $reffile..."
-    #echo -n "basedir: $basedir..."
+    echo -n "$basename..."
 
     echo 1>&2
     echo "###### Testing $basename" 1>&2
 
-    generatedfiles="$generatedfiles ${basename}.out ${basename}.diff" &&
-    $GAL < $1 2> "${basename}.out" ">>" $globallog &&
-    Compare "${basename}.out" "$basedir$basename.err" ${basename}.diff
+    generatedfiles=""
+
+    generatedfiles="$generatedfiles ${basename}.err ${basename}.diff" &&
+    RunFail "$GAL" "<" $1 "2>" "${basename}.err" ">>" $globallog &&
+    Compare ${basename}.err ${reffile}.err ${basename}.diff
+
+    # Report the status and clean up the generated files
 
     if [ $error -eq 0 ] ; then
     if [ $keep -eq 0 ] ; then
@@ -122,8 +134,29 @@ CheckFail()
     echo "###### FAILED" 1>&2
     globalerror=$error
     fi
-
 }
+
+while getopts kdpsh c; do
+    case $c in
+    k) # Keep intermediate files
+        keep=1
+        ;;
+    h) # Help
+        Usage
+        ;;
+    esac
+done
+
+shift `expr $OPTIND - 1`
+
+LLIFail() {
+  echo "Could not find the LLVM interpreter \"$LLI\"."
+  echo "Check your LLVM installation and/or modify the LLI variable in testall.sh"
+  exit 1
+}
+
+which "$LLI" >> $globallog || LLIFail
+
 
 if [ $# -ge 1 ]
 then
@@ -146,7 +179,6 @@ do
         globalerror=1
         ;;
     esac
-    
 done
 
-cat $globallog
+exit $globalerror
